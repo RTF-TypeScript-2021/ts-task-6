1f47b8c78d966277838d58d57273c20e4b95f639
Nikita Zerniukov <n.zernyukov@artsofte.ru>
2021-11-23 16:49:15 +0500

    4+

--- src/task-4/index.ts
+++ src/task-4/index.ts
@@ -1,3 +1,16 @@
 /**
- * 
+ * Задание 4 - Reflect Metadata
+ * Напишите декоратор FieldCount который через Reflection API получает информацию о полях класса и
+ * сохраняет их в поле fields
  */
+
+class SimpleExample{
+    @FieldCount()
+    public name: string;
+    @FieldCount()
+    public age: number;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
