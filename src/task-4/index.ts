637e638f71892a855dbea5b8be0c5f45d2de2db0
predeinnikita <54530655+Nikita4513@users.noreply.github.com>
2021-11-29 19:32:48 +0500

    не понял 4 таск
    Закомментированные строки кода - места где компилятор ругается

--- src/task-4/index.ts
+++ src/task-4/index.ts
@@ -4,13 +4,26 @@
  * сохраняет их в поле fields
  */
 
+ import 'reflect-metadata';
+
 class SimpleExample{
-    @FieldCount()
+    @FieldCount
     public name: string;
-    @FieldCount()
+    @FieldCount
     public age: number;
 
     constructor(name: string) {
         this.name = name;
     }
 }
+
+function FieldCount(target: Object, propertyKey: string | symbol): void {
+    if (!('fields' in target)) {
+        //target["fields"] = new Map<string | symbol, any>();
+    }
+
+    //target['fields'].set(propertyKey, Reflect.getMetadata('design:type', target, propertyKey));
+}
+
+const s = new SimpleExample('a');
+//console.log(s.fields.count);
