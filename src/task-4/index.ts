0e9612ec33356838319b7f19501635a7398c40ce
Darctys <yltimatym2002@maiil.ru>
2021-11-29 18:36:29 +0500

    Сычев Тимофей

--- src/task-4/index.ts
+++ src/task-4/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 /**
  * Задание 4 - Reflect Metadata
  * Напишите декоратор FieldCount который через Reflection API получает информацию о полях класса и
@@ -5,12 +6,27 @@
  */
 
 class SimpleExample{
-    @FieldCount()
+    @FieldCount
     public name: string;
-    @FieldCount()
+
+    @FieldCount
     public age: number;
 
     constructor(name: string) {
         this.name = name;
+
     }
 }
+
+function FieldCount(target: Object, key: string) {
+    const type = Reflect.getMetadata("design:type",target, key);
+    // Переопределяем декорируемое свойство
+    if (target.hasOwnProperty('fields')){
+        target.fields.set(key,type);
+    }else{
+        target['fields'] = new Map([[key,  type]]);
+    }
+}
+
+const cl = new SimpleExample('tim');
+console.log(cl.fields);
