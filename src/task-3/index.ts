437c50699c914c8ac1c55fd3bb987ae1dd2d7b1e
Sychqh <sychqh@gmail.com>
2021-11-29 20:09:57 +0500

    Сделал 3 задание

--- src/task-3/index.ts
+++ src/task-3/index.ts
@@ -1,17 +1,20 @@
-import { LogClassInstance } from '../task-1';
-
 /**
  * Задание 3. No more extensions!
  * Реализуйте декоратор класса, который предотвратит дальнейшее добавление/изменение функций класса.
  */
+import {LogClassInstance} from '../task-1';
 
 @LogClassInstance
 @PreventExtensions
-export class PartlyExtendableCreature{
+export class PartlyExtendableCreature {
     public readonly name: string;
-
+    
     constructor(name: string) {
         this.name = name;
     }
 }
 
+function PreventExtensions<T extends { new(...args: any[]): object }>(target: T) {
+    Object.seal(target);
+    Object.seal(target.prototype);
+}
