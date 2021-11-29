154c82ff270c9c58056d2fd82025cc9c4559ff22
Sychqh <sychqh@gmail.com>
2021-11-29 20:04:49 +0500

    Сделал 2 задание

--- src/task-2/index.ts
+++ src/task-2/index.ts
@@ -4,10 +4,17 @@
  * который приводит переданный аргумент в метод, до ближайшего целого числа.
  *
  */
-
-export class Square{
+export class Square {
     @OnlyInteger
-    public getArea(sideLength: number){
+    public getArea(sideLength: number) {
         return sideLength * sideLength;
+    }
+}
+
+export function OnlyInteger(target: object, key: string, descriptor: PropertyDescriptor) {
+    const original = descriptor.value as (num: number) => number;
+    
+    descriptor.value = function (num: number): number {
+        return original.call(this, Math.round(num)) as number;
     }
 }
