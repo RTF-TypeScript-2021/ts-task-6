3043e5f5872f4c59f6b8a9d37fc37ca739fa404d
Angelina Shkatova <shkatova.angel@yandex.ru>
2021-11-29 09:28:25 +0500

    Task 1 is solved

--- src/task-1/index.ts
+++ src/task-1/index.ts
@@ -1,15 +1,25 @@
 /**
- * Задание 1. Логгирование создание класса
- * Реализуйте декоратор @LogClassInstance который будет логировать создание обьекта в консоль.
+ * Задание 1. Логирование создание класса
+ * Реализуйте декоратор @LogClassInstance который будет логировать создание объекта в консоль.
  * Формат лога: SimpleCreature created with args : ...${args}
  * Пример: SimpleCreature created with args: Пушок, 12
- */
+ * */
+
+function LogClassInstance<TFunction extends Function>(target: TFunction): TFunction {
+
+    const newConstructor: Function = function (name: string, age: number) {
+        console.log(`SimpleCreature created with args: ${name}, ${age}`);
+        this.name = name;
+        this.age = age;
+    }
+
+    return <TFunction>newConstructor;
+}
 
 @LogClassInstance
-export class SimpleCreature{
+export class SimpleCreature {
     public readonly name: string;
     public readonly age: number;
-
 
     constructor(name: string, age: number) {
         this.name = name;
@@ -17,6 +27,6 @@ export class SimpleCreature{
     }
 }
 
-
-export function LogClassInstance(){
-}
+const python = new SimpleCreature("Boris", 56);
+const sandy = new SimpleCreature("Sandy", 15);
+console.log(python, sandy);
